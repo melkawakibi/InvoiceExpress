fbc03652bb685e67769a9470070756d299625b19
Mohamed El Kawakibi <kawakim001@hva.nl>
2017-05-13 20:19:40 +0200

    login system

---
+++ view/login.php
@@ -0,0 +1,62 @@
+<html>
+<head>
+	<title></title>
+	<?php 
+		include 'parts/links.php'; 
+	?> 
+</head>
+<body>
+
+<div class="container">
+	<?php
+		include 'parts/header.php';
+	?>
+<div class="page-header">
+<h3>Login</h3>
+</div>
+
+	<div class="col-md-4">
+		<form action="login.php" method="post">
+		  <div class="form-group">
+		    <label for="username">username</label>
+		    <input type="text" class="form-control" name="username">
+		  </div>
+		  <div class="form-group">
+		    <label for="password">password</label>
+		    <input type="password" class="form-control" name="password">
+		  </div>
+		   <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+		</form>	
+	</div>
+
+</container>
+
+</body>
+</html>
+
+<?php
+
+$db = new Database();
+
+if(isset($_POST['submit'])){
+	
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+	$db->query("SELECT * FROM user WHERE username='$username' AND password='$password'");
+
+	$db->execute();
+
+	$count = $db->rowCount();
+
+	if($count == 1){
+		session_start();
+		$_SESSION['user'] = $username;
+		echo '<script> location.replace("index.php"); </script>';
+	}else{
+		echo 'Wrong credentials';
+	}
+
+}
+
+?>
--- view/parts/nav.php
+++ view/parts/nav.php
@@ -1,3 +1,12 @@
-<nav class="navbar-right">
-	<a class="nav-link" href="../login.php">login</a>
-</nav>
+<?php
+	session_start();
+
+	if(isset($_SESSION['user'])){
+		echo $_SESSION['user'];
+		echo '<nav class="nav" style="float: right;">
+			<a class="nav-link" href="parts/logout.php">logout</a></nav>';
+	}else{
+		echo '<nav class="nav" style="float: right;">
+			<a class="nav-link" href="../login.php">login</a></nav>';
+	}
+?>
